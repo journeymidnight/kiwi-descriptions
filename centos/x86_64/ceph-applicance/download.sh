a9cc63b80c7703187113168b76444bd847734d08
Bamvor Zhang <bamv2005@gmail.com>
2018-01-09 03:24:29 -0500

    parse json with jq and download private repo with curl
    curl -H "Accept: application/octet-stream". ref <https://developer.github.com/v3/repos/releases/#get-a-single-release-asset>
    
    Signed-off-by: Bamvor Zhang <bamv2005@gmail.com>

--- centos/x86_64/ceph-applicance/download.sh
+++ centos/x86_64/ceph-applicance/download.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
 account="bjzhang"
-token="2534b89f255c4fc100aaee5ec5b6cb152256e659"
+token="4de98236b1126a727da929d2c9aeac8339fbac82"
+
+download_asset()
+{
+	user=$1
+	repo=$2
+	json=$3
+	filelist=$4
+	ids=`echo $json | jq '.assets[]' | jq "[.id][]"`
+	for id in $ids; do
+		name=`echo $json | jq '.assets[]' | jq "[.id, .name][]" | grep $id -A1 | tail -n1 | sed "s/\"//g"`
+		if [ "$filelist" != "" ]; then
+			for want in $filelist; do
+				echo "${name##*/}" | grep "^$want\-[0-9].*$" > /dev/null
+				ret=$?
+				if [ "$ret" = "0" ]; then
+					echo "downloading $name"
+					curl -o $name -L -H "Accept: application/octet-stream" -u $account:$token https://api.github.com/repos/$user/$repo/releases/assets/$id
+					break
+				else
+					continue
+				fi
+			done
+		else
+			echo "downloading $name"
+			curl -o $name -L -H "Accept: application/octet-stream" -u $account:$token https://api.github.com/repos/$user/$repo/releases/assets/$id
+		fi
+	done
+}
 
 download_rpms()
 {
@@ -9,24 +37,8 @@ download_rpms()
 	repo=$2
 	type=$3
 	filelist=$4
-	files=`curl -u $account:$token -s https://api.github.com/repos/$user/$repo/releases/latest | grep "browser_download_url" | grep "\.$type" | cut -d \" -f 4`
-	for f in $files; do
-		if [ "$filelist" != "" ]; then
-			for want in $filelist; do
-				echo "${f##*/}" | grep "^$want\-[0-9].*$" > /dev/null
-				if [ "$?" = 0 ]; then
-					echo "$downloading $f"
-					curl -O -u $account:$token -L $f
-					break
-				else
-					continue
-				fi
-			done
-		else
-			echo "$downloading $f"
-			curl -O -u $account:$token -L $f
-		fi
-	done
+	json=`curl -u $account:$token -s https://api.github.com/repos/$user/$repo/releases/latest`
+	download_asset $user $repo "$json" "$filelist"
 }
 
 download_all_tars()
@@ -39,13 +51,14 @@ download_all_tars()
 }
 
 cd rpms
+rm * -rf
 download_rpms journeymidnight nier rpm
-#download_rpms journeymidnight niergui rpm
+download_rpms journeymidnight niergui rpm
 download_rpms journeymidnight automata rpm
-#download_rpms journeymidnight prometheus-rpm rpm "ceph_exporter node-exporter prometheus"
+download_rpms journeymidnight prometheus-rpm rpm "ceph_exporter node-exporter prometheus"
 cd ..
-#cd binaries
-#download_all_tars journeymidnight storedeployer
+cd binaries
+download_all_tars journeymidnight storedeployer
 #curl -O -L http://download.pingcap.org/tidb-latest-linux-amd64.tar.gz
 #curl -O -L http://download.pingcap.org/tidb-latest-linux-amd64.sha256
 #sha256sum -c tidb-latest-linux-amd64.sha256
@@ -53,5 +66,5 @@ cd ..
 #	echo "download tidb failed. exit!"
 #	exit 127
 #fi
-#cd ..
-#tar zcvf binaries.tar.gz binaries
+cd ..
+tar zcvf binaries.tar.gz binaries
