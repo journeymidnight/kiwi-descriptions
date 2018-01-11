7b90d81c2740df4adbcbcfad892a62946088507f
Bamvor Zhang <bamv2005@gmail.com>
2018-01-11 01:21:51 -0500

    tidb: support tidb environment: NOT TEST YET
    Signed-off-by: Bamvor Zhang <bamv2005@gmail.com>

--- centos/x86_64/ceph-applicance/config.sh
+++ centos/x86_64/ceph-applicance/config.sh
@@ -19,6 +19,21 @@ baseMount
 # Activate services
 #--------------------------------------
 #...
+systemctl enable ntpd
+systemctl stop firewalld
+deploy_user=tidb
+echo "Configuration system and user limitation"
+sysctl -w net.core.somaxconn=32768
+sysctl -w vm.swappiness=0
+sysctl -w net.ipv4.tcp_syncookies=0
+sysctl -w fs.file-max=1000000
+LIMITS_CONF="/etc/security/limits.conf"
+echo "$deploy_user        soft        nofile        1000000" >> $LIMITS_CONF
+echo "$deploy_user        hard        nofile        1000000" >> $LIMITS_CONF
+echo "$deploy_user        soft        core          unlimited"
+>> $LIMITS_CONF
+echo "$deploy_user        soft        stack         10240" >> $
+LIMITS_CONF
 
 #======================================
 # Setup default target, multi-user
