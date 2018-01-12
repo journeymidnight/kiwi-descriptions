75ab763a6256861cf26a85e2bd2f2ef4ab74a5fe
Bamvor Zhang <bamv2005@gmail.com>
2018-01-12 03:42:34 -0500

    ceph: input token instead of uploading token to github which will be deleted by github
    Signed-off-by: Bamvor Zhang <bamv2005@gmail.com>

--- centos/x86_64/ceph-applicance/build.sh
+++ centos/x86_64/ceph-applicance/build.sh
@@ -2,10 +2,17 @@
 
 s3_repo_dir=/var/cache/kiwi/yum/cache/repo_samba
 s3_repo_url=http://reposamba.los-cn-north-1.lecloudapis.com
+token=$1
+
+if [ "$token" = "" ]; then
+	echo "github api token mission, exit"
+	exit 127
+fi
 
 echo "Downloading repo data from fake repo in s3"
 mkdir -p $s3_repo_dir
 cd $s3_repo_dir
+rm -rf  $s3_repo_url/repodata/repomd.xml $s3_repo_dir/[0-9]*
 wget $s3_repo_url/repodata/repomd.xml
 grep location  repomd.xml  | sed "s/^.*\"\(.*\)\".*$/wget http:\/\/reposamba.los-cn-north-1.lecloudapis.com\/\1/g" > repomd.sh
 bash ./repomd.sh
@@ -13,7 +20,7 @@ cd ..
 
 echo "Downloading rpm and binaries(skip TiDB as we do not udpate when necessary)"
 cd /home/suse01/works/source/kiwi-descriptions/centos/x86_64/ceph-applicance
-bash ./download.sh
+bash ./download.sh $token
 cd rpms
 rm repodata -rf
 createrepo .
