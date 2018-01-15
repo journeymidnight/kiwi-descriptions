2123d42a829540babc16aeec1bbf6181ecbc8d5e
Bamvor Zhang <bamv2005@gmail.com>
2018-01-15 08:14:26 -0500

    ceph: fix the permission for directory in tidb; add mysql
    Signed-off-by: Bamvor Zhang <bamv2005@gmail.com>

--- centos/x86_64/ceph-applicance/config.sh
+++ centos/x86_64/ceph-applicance/config.sh
@@ -30,7 +30,9 @@ echo "$deploy_user        soft        core          unlimited" >> $LIMITS_CONF
 echo "$deploy_user        soft        stack         10240" >> $LIMITS_CONF
 mv /binaries/tidb-latest-linux-amd64/bin/ /home/tidb/deploy
 mkdir -p /home/tidb/deploy/log
+chown tidb:tidb /home/tidb/deploy/log -R
 mkdir -p /home/tidb/deploy/status
+chown tidb:tidb /home/tidb/deploy/status -R
 
 #======================================
 # Setup default target, multi-user
