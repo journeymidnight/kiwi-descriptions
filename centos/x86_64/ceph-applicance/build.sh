42838c1fb63e9f83e3460e5464d81dbbeb9d7045
Bamvor Jian Zhang <bamv2005@gmail.com>
2018-01-26 10:07:53 +0100

    ceph helper: use home variable instead of fix variable
    Signed-off-by: Bamvor Jian Zhang <bamv2005@gmail.com>

--- centos/x86_64/ceph-applicance/build.sh
+++ centos/x86_64/ceph-applicance/build.sh
@@ -3,6 +3,7 @@
 s3_repo_dir=/var/cache/kiwi/yum/cache/repo_samba
 s3_repo_url=http://reposamba.los-cn-north-1.lecloudapis.com
 token=$1
+home=/home/vagrant
 
 if [ "$token" = "" ]; then
 	echo "github api token mission, exit"
@@ -19,19 +20,19 @@ bash ./repomd.sh
 cd ..
 
 echo "Downloading rpm and binaries(skip TiDB as we do not udpate when necessary)"
-cd /home/suse01/works/source/kiwi-descriptions/centos/x86_64/ceph-applicance
+cd $home/works/source/kiwi-descriptions/centos/x86_64/ceph-applicance
 #bash ./download.sh $token
 cd rpms
 rm repodata -rf
 createrepo .
 
 echo "Cleaning up the previous build"
-mv ~/works/software/kiwi/build/image-root/var/lib/machines/ ~/works/software/kiwi.machines.old-`date "+%d%m%S"`
-rm -rf ~/works/software/kiwi/build/image-root/
+mv $home/works/software/kiwi/build/image-root/var/lib/machines/ $home/works/software/kiwi.machines.old-`date "+%d%m%S"`
+rm -rf $home/works/software/kiwi/build/image-root/
 systemctl restart lvm2-lvmetad.service
 
 echo "Building(comment --debug if you want to a clean output)"
-kiwi-ng --debug --color-output --type oem system build --description /home/suse01/works/source/kiwi-descriptions/centos/x86_64/ceph-applicance --target-dir ~/works/software/kiwi
+kiwi-ng --debug --color-output --type oem system build --description $home/works/source/kiwi-descriptions/centos/x86_64/ceph-applicance --target-dir $home/works/software/kiwi
 if [ "$?" = "0" ]; then
 	echo "Building Successful"
 else
